71d6adb9bc0d9c77aad7f1cd346e97f9ab1245f3
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-02 10:46:49 -0800

    adding program file

---
+++ UC10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpUC10
+{
+     class CompanyEmpWage
+    {
+
+        private string company;
+        public int emprateperhr;
+        public int NumOfWorkingPerDays;
+        public int MaxHourPerMonth;
+        public int TotalEmpWage;
+
+
+        public CompanyEmpWage(string company, int emprateperhr, int NumOfWorkingDays, int MaxHourPerMonth)
+        {
+            this.company = company;
+            this.emprateperhr = emprateperhr;
+            this.NumOfWorkingPerDays = NumOfWorkingPerDays;
+            this.MaxHourPerMonth = MaxHourPerMonth;
+
+        }
+
+
+        public void setTotalEmpWage(int TotalEmpwage)
+        {
+            this.TotalEmpWage = TotalEmpWage;
+        }
+
+        
+
+
+
+    }
+}
