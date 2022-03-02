a2d20ee45be45459705654a5ea283263bb50e1d6
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-02 10:53:04 -0800

    adding Object creation file

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
     public static void Main(string[] args)
     {
         EmpBui Build = new EmpBui();
-        Build.U10C("Dmart", 2, 3, 4);
+        Build.U10C("Dmart", 2, 3, 4,0);
         Build.computeEmpWage();
     }
 }
