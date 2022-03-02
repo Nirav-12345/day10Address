b7f6f88053971c640dc8e7273c3050317d077a84
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-02 10:42:40 -0800

    adding class file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpUC9;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        U9C dmart = new U9C("twtwt",20,2,10);
+
+        dmart.ComputeWage();
+    }
+}
